d2f291b35b2f1c6f5c94e8b0a8d66dc60334d976
Aayush0821 <vatsayush67@gmail.com>
2026-06-28 18:53:50 +0530

    feat(Resume): add music-enabled buttons and detailed resume sections in CorkBoard

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -43,7 +43,7 @@ export default function Portfolio({
 
       <Experience />
 
-      <Resume />
+      <Resume musicEnabled={musicEnabled}/>
     </main>
   );
 }
--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -3,12 +3,16 @@
 import CorkBoard from "./CorkBoard";
 import ResumeHeading from "./ResumeHeading";
 
-const Resume = () => {
+const Resume = ({
+    musicEnabled=false
+}:{
+    musicEnabled?:boolean
+}) => {
   return (
     <section id="resume" className="min-h-screen px-4 pt-21 pb-5">
       <div className="mx-auto max-w-6xl">
         <ResumeHeading />
-        <CorkBoard />
+        <CorkBoard musicEnabled={musicEnabled}/>
       </div>
     </section>
   );
